6f2b3c24534132293a873a3329af88f6f69b0eb0
surprise2024-cpu <surprise2024@gmail.com>
2026-06-06 20:13:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log('Fetched data:', data);
+        // Process the fetched data
+        const summaryContainer = document.querySelector('.summary');
+
+        summaryContainer.querySelectorAll('.category').forEach(element => element.remove());
+
+        data.forEach(item => {
+
+            const categoryElement = document.createElement('div');
+
+            categoryElement.classList.add('category', item.category.toLowerCase());
+
+            categoryElement.innerHTML = `
+                <span class="icon">
+                    <img src="${item.icon}" alt="${item.category} icon" class="icon">
+                </span>
+                <span class="testType">
+                    ${item.category}
+                </span>
+                <span class="value">
+                    ${item.score}
+                    <span class="mark"> / 100</span>    
+                </span>
+            `;
+
+            summaryContainer.insertBefore(categoryElement, summaryContainer.querySelector('.dividerBtn'));
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
